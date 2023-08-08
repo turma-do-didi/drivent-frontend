7ba636b5bc195c208988b3cb87911c92b04329bd
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-08-08 00:06:37 -0300

    feat: fetching user bookings

--- src/components/HotelReservation/index.js
+++ src/components/HotelReservation/index.js
@@ -5,23 +5,31 @@ import BlockedBooking from './BlockedBooking';
 import HotelListing from './HotelListing';
 import { useState } from 'react';
 import RoomListing from './RoomListing';
+import useBooking from '../../hooks/api/useBooking';
 
 export default function HotelReservation() {
+  const { booking } = useBooking();
   const hotel = useHotels();
   const [selectedHotel, setSelectedHotel] = useState(null);
   const [selectedRoom, setSelectedRoom] = useState(null);
+
+  console.log(booking);
 
   function hotelContent() {
     if (hotel.hotelsError) {
       return (
         <BlockedBooking error={hotel.hotelsError} />
       );
-    } else if(!hotel.hotelsError) {
+    } else if(!hotel.hotelsError && !booking) {
       return (
         <>
           <HotelListing hotelList={hotel.hotels} selectedHotel={selectedHotel} setSelectedHotel={setSelectedHotel} setSelectedRoom={setSelectedRoom} />
           {selectedHotel ? <RoomListing rooms={selectedHotel.Rooms} selectedRoom={selectedRoom} setSelectedRoom={setSelectedRoom} /> : null}
         </>
+      );
+    } else if (!hotel.hotelsError && booking) {
+      return (
+        <h1>Quem ja tem reserva cai aq</h1>
       );
     }
   }
--- src/services/bookingApi.js
+++ src/services/bookingApi.js
@@ -8,3 +8,12 @@ export async function save(body, token) {
   });
   return response.data;
 }
+
+export async function getUserBooking(token) {
+  const response = await api.get('/booking', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+}
